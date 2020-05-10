4c3d1221b846ce9098d6d7980a7cd88201e29bc6
CarlosCarmona <carloscarmonaalcantara@gmail.com>
2020-05-10 19:05:06 +0200

    build: 2.0.0

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# HTTP executor for [Runnerty]:
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">A new way for processes managing</p>
+</p>
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency Status][david-badge]][david-badge-url] 
+<a href="#badge">
+  <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg">
+</a>
+
+# Executor for [Runnerty]: HTTP
 
 ### Configuration sample:
 ```json
@@ -11,70 +23,62 @@
 ### Plan sample:
 ```json
 {
-  "id":"http_default",
-  "headers":{"User-Agent": "runnerty"},
-  "method":"GET",
-  "uri":"https://api.github.com/search/repositories",
-  "qs":{"q": "runnerty"},
-  "json": true
+  "id": "http_default",
+  "headers": {"User-Agent": "runnerty"},
+  "method": "get",
+  "url": "https://api.github.com/search/repositories",
+  "params":{"q": "runnerty"},
+  "responseType": "json"
 }
 ```
 
 ```json
 {
-  "id":"http_default",
-  "headers":{"User-Agent": "runnerty"},
-  "method":"GET",
-  "uri":"https://api.twitter.com/1.1/users/show.json",
-  "oauth":
-    { "consumer_key": "...",
-      "consumer_secret": "...",
-      "token": "...",
-      "token_secret": "..."
-    },
-  "qs":{"screen_name": "runnerty"},
-  "json": true
-}
-```
-
-```json
-{
-  "id":"http_default",
-  "headers":{"User-Agent": "runnerty"},
-  "uri":"http://www.sample.com/form",
-  "method":"POST",
-  "form": {
+  "id": "http_default",
+  "headers": {"User-Agent": "runnerty"},
+  "url": "http://www.sample.com/form",
+  "method":"post",
+  "data": {
     "key1": "value1",
     "key2": "value2"
   },
-  "json": true
+  "responseType": "json"
 }
 ```
 
 ```json
 {
-  "id":"http_default",
-  "headers":{"User-Agent": "runnerty"},
-  "uri":"http://www.sample.com/uploadfile",
-  "method":"POST",
+  "id": "http_default",
+  "headers": { "User-Agent": "runnerty" },
+  "url": "http://www.sample.com/uploadfile",
+  "method": "post",
   "files":[{"name":"fileOne", "path":"/var/myfile.txt"},
            {"name":"fileTwo", "path":"/var/www/runnerty.jpg"}],
-  "json": true
+  "responseType": "json"
 }
 ```
 
 ```json
 {
-  "id":"http_default",
-  "headers":{"User-Agent": "runnerty", "Content-Type": "application/xml"},
-  "method":"POST",
-  "uri":"https://sample.com/api-sample",
+  "id": "http_default",
+  "headers": { "User-Agent": "runnerty", "Content-Type": "application/xml" },
+  "method": "post",
+  "url": "https://sample.com/api-sample",
   "auth": {
-    "user": ":MY_USER_AUTH",
-    "pass": ":MY_PASS_AUTH"
+    "user": "@GV(:MY_USER_AUTH)",
+    "pass": "@GV(:MY_PASS_AUTH)"
   },
-  "body": ":SAMPLE_BODY"
+  "data": "@GV(:SAMPLE_BODY)"
 }
 ```
 
+### Output (Process values):
+* `PROCESS_EXEC_DATA_OUTPUT`: Response output data.
+* `PROCESS_EXEC_ERR_OUTPUT`: Error output message.
+
 [Runnerty]: http://www.runnerty.io
+[downloads-image]: https://img.shields.io/npm/dm/@runnerty/executor-http.svg
+[npm-url]: https://www.npmjs.com/package/@runnerty/executor-http
+[npm-image]: https://img.shields.io/npm/v/@runnerty/executor-http.svg
+[david-badge]: https://david-dm.org/runnerty/executor-http.svg
+[david-badge-url]: https://david-dm.org/runnerty/executor-http
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
-
-const http = require('request');
+const http = require('http');
+const https = require('https');
+const axios = require('axios');
+const formData = require('form-data');
+const qs = require('qs');
 const fs = require('fs');
 
 const Execution = global.ExecutionClass;
@@ -13,11 +16,25 @@ class httpExecutor extends Execution {
   exec(values) {
     let endOptions = { end: 'end' };
 
-    if (values.agentOptions) {
+    // HTTPS-Agent
+    if (values.httpsAgent) {
       try {
-        if (values.agentOptions.ca_file) values.agentOptions.ca = fs.readFileSync(values.agentOptions.ca_file);
-        if (values.agentOptions.cert_file) values.agentOptions.cert = fs.readFileSync(values.agentOptions.cert_file);
-        if (values.agentOptions.pfx_file) values.agentOptions.pfx = fs.readFileSync(values.agentOptions.pfx_file);
+        const httpsAgentParams = values.httpsAgent;
+        if (httpsAgentParams.key_file) httpsAgentParams.key = fs.readFileSync(httpsAgentParams.key_file);
+        if (httpsAgentParams.ca_file) httpsAgentParams.ca = fs.readFileSync(httpsAgentParams.ca_file);
+        if (httpsAgentParams.cert_file) httpsAgentParams.cert = fs.readFileSync(httpsAgentParams.cert_file);
+        if (httpsAgentParams.pfx_file) httpsAgentParams.pfx = fs.readFileSync(httpsAgentParams.pfx_file);
+
+        const httpsAgentOptions = {
+          servername: httpsAgentParams.servername,
+          passphrase: httpsAgentParams.passphrase,
+          ca: httpsAgentParams.ca,
+          cert: httpsAgentParams.cert,
+          key: httpsAgentParams.key,
+          pfx: httpsAgentParams.pfx,
+          maxCachedSessions: httpsAgentParams.maxCachedSessions
+        };
+        values.httpsAgent = new https.Agent(httpsAgentOptions);
       } catch (err) {
         endOptions.end = 'error';
         endOptions.messageLog = err;
@@ -26,28 +43,50 @@ class httpExecutor extends Execution {
       }
     }
 
-    var req = http(values, (err, response, body) => {
-      if (err) {
+    // HTTP-Agent
+    if (values.httpAgent) {
+      try {
+        values.httpAgent = new http.Agent(values.httpAgent);
+      } catch (err) {
         endOptions.end = 'error';
         endOptions.messageLog = err;
         endOptions.err_output = err;
         this.end(endOptions);
       }
-
-      endOptions.end = 'end';
-      endOptions.data_output = body;
-      this.end(endOptions);
-    });
-
-    if (values.method === 'POST' || values.method === 'PUT') {
-      if (values.files) {
-        var form = req.form();
-        let filesLength = values.files.length;
-        for (let i = 0; i < filesLength; i++) {
-          form.append(values.files[i].name, fs.createReadStream(values.files[i].path));
-        }
-      }
     }
+
+    // FILES FORM
+    if (values.files && (values.method === 'post' || values.method === 'put')) {
+      const form = new formData();
+      let filesLength = values.files.length;
+      for (let i = 0; i < filesLength; i++) {
+        form.append(values.files[i].name, fs.createReadStream(values.files[i].path));
+      }
+      values.data = form;
+      const formHeaders = form.getHeaders();
+      values.headers = { ...formHeaders, ...values.headers };
+    }
+
+    // PARAMS-SERIALIZER
+    if (values.paramsSerializerOptions) {
+      const paramsSerializerOptions = values.paramsSerializerOptions;
+      values.paramsSerializer = params => {
+        return qs.stringify(params, paramsSerializerOptions);
+      };
+    }
+
+    axios(values)
+      .then(response => {
+        endOptions.end = 'end';
+        endOptions.data_output = response.data;
+        this.end(endOptions);
+      })
+      .catch(err => {
+        endOptions.end = 'error';
+        endOptions.messageLog = err;
+        endOptions.err_output = err;
+        this.end(endOptions);
+      });
   }
 }
 
